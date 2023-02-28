43cc37db1c300c2e4f01e9a9e283a785cca49853
Billy <bimganda@gmail.com>
2023-02-28 03:36:27 -0500

    created and implemented IStripeAppService

---
+++ StripeAPI/Application/StripeAppService.cs
@@ -0,0 +1,29 @@
+﻿using Stripe;
+using StripeAPI.Contracts;
+using StripeAPI.Models.Stripe;
+
+namespace StripeAPI.Application
+{
+    public class StripeAppService : IStripeAppService
+    {
+        private readonly ChargeService _chargeService;
+        private readonly CustomerService _customerService;
+        private readonly TokenService _tokenService;
+        public StripeAppService(ChargeService chargeService, CustomerService customerService, TokenService tokenService)
+        {
+            _chargeService = chargeService;
+            _customerService = customerService;
+            _tokenService = tokenService;
+        }
+
+        public Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StripeAPI/Contracts/IStripeAppService.cs
@@ -0,0 +1,10 @@
+﻿using StripeAPI.Models.Stripe;
+
+namespace StripeAPI.Contracts
+{
+    public interface IStripeAppService
+    {
+        Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken ct);
+        Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken ct);
+    }
+}
