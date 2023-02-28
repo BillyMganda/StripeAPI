513a4452685056551973e3b56f0b1b34cc650283
Billy <bimganda@gmail.com>
2023-02-28 03:48:23 -0500

    added [HttpPost("customer/add")]

--- StripeAPI/Controllers/StripeController.cs
+++ StripeAPI/Controllers/StripeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StripeAPI.Contracts;
+using StripeAPI.Models.Stripe;
 
 namespace StripeAPI.Controllers
 {
@@ -7,5 +9,17 @@ namespace StripeAPI.Controllers
     [ApiController]
     public class StripeController : ControllerBase
     {
+        private readonly IStripeAppService _stripeService;
+        public StripeController(IStripeAppService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+        [HttpPost("customer/add")]
+        public async Task<ActionResult<StripeCustomer>> AddStripeCustomer([FromBody] AddStripeCustomer customer, CancellationToken ct)
+        {
+            StripeCustomer createdCustomer = await _stripeService.AddStripeCustomerAsync(customer, ct);
+            return StatusCode(StatusCodes.Status200OK, createdCustomer);
+        }
     }
 }
