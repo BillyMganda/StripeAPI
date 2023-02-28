937925a4e3479f8f6cdbd7981b6e4f97d097c3a7
Billy <bimganda@gmail.com>
2023-02-28 03:50:21 -0500

    added [HttpPost("payment/add")]

--- StripeAPI/Controllers/StripeController.cs
+++ StripeAPI/Controllers/StripeController.cs
@@ -21,5 +21,12 @@ namespace StripeAPI.Controllers
             StripeCustomer createdCustomer = await _stripeService.AddStripeCustomerAsync(customer, ct);
             return StatusCode(StatusCodes.Status200OK, createdCustomer);
         }
+
+        [HttpPost("payment/add")]
+        public async Task<ActionResult<StripePayment>> AddStripePayment([FromBody] AddStripePayment payment, CancellationToken ct)
+        {
+            StripePayment createdPayment = await _stripeService.AddStripePaymentAsync(payment, ct);
+            return StatusCode(StatusCodes.Status200OK, createdPayment);
+        }
     }
 }
