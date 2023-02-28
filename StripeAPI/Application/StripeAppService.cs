ade246d88f146e8dc9305644d25c681a496fa54c
Billy <bimganda@gmail.com>
2023-02-28 03:40:28 -0500

    implemented AddStripeCustomerAsync

--- StripeAPI/Application/StripeAppService.cs
+++ StripeAPI/Application/StripeAppService.cs
@@ -16,9 +16,37 @@ namespace StripeAPI.Application
             _tokenService = tokenService;
         }
 
-        public Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken ct)
+        public async Task<StripeCustomer> AddStripeCustomerAsync(AddStripeCustomer customer, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            // Set Stripe Token options based on customer data
+            TokenCreateOptions tokenOptions = new TokenCreateOptions
+            {
+                Card = new TokenCardOptions
+                {
+                    Name = customer.Name,
+                    Number = customer.CreditCard.CardNumber,
+                    ExpYear = customer.CreditCard.ExpirationYear,
+                    ExpMonth = customer.CreditCard.ExpirationMonth,
+                    Cvc = customer.CreditCard.Cvc
+                }
+            };
+
+            // Create new Stripe Token
+            Token stripeToken = await _tokenService.CreateAsync(tokenOptions, null, ct);
+
+            // Set Customer options using
+            CustomerCreateOptions customerOptions = new CustomerCreateOptions
+            {
+                Name = customer.Name,
+                Email = customer.Email,
+                Source = stripeToken.Id
+            };
+
+            // Create customer at Stripe
+            Customer createdCustomer = await _customerService.CreateAsync(customerOptions, null, ct);
+
+            // Return the created customer at stripe
+            return new StripeCustomer(createdCustomer.Name, createdCustomer.Email, createdCustomer.Id);
         }
 
         public Task<StripePayment> AddStripePaymentAsync(AddStripePayment payment, CancellationToken ct)
